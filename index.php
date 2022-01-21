eb0757f41ab1a7f63ae528955eb4bd9d43794b9b
wildarte <wildarte10@gmail.com>
2022-01-21 19:13:53 -0300

    primeiro commit do site, criacao do header

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>blog - VIMMPag</title>
+</head>
+<body>
+    
+    <header class="header container-full">
+        <div class="container-full header_content">
+            <div class="header_over container-full">
+                <div class="container header_over_container">
+                    <ul class="header_over_contato">
+                        <li><a href="mailto:contato@meusite.com.br"><i class="bi bi-envelope"></i>contato@meusite.com.br</a></li>
+                        <li><a href="tel:+5511999999999"><i class="bi bi-telephone"></i> +55 11 9 9999-9999</a></li>
+                    </ul>
+                    <ul class="header_over_social">
+                        <li class="icon-linkedin"><a href="http://"><i class="bi bi-linkedin"></i></a></li>
+                        <li class="icon-facebook"><a href="http://"><i class="bi bi-facebook"></i></a></li>
+                        <li class="icon-instagram"><a href="http://"><i class="bi bi-instagram"></i></a></li>
+                    </ul>
+                </div>     
+            </div>
+            <div class="header_bar container-full">
+                <div class="header_bar_content container">
+                    <div class="header_link_logo">
+                        <a href="">
+                            <img src="./assets/img/logo.png" alt="">
+                        </a>
+                    </div>
+                    <nav class="menu">
+                        <ul>
+                            <li><a href="http://">Site</a></li>
+                            <li><a href="http://">Sobre</a></li>
+                            <li><a href="http://">Contato</a></li>
+                        </ul>
+                    </nav>
+                    <div class="header_cta">
+                        <a href="http://">
+                            Simule Agora
+                        </a>
+                    </div>
+                </div>   
+            </div>
+        </div>
+    </header>
+    <div class="home_intro">
+        <h1>Blog</h1>
+    </div>
+
+    <main>
+        
+        <section>
+            <article>
+                <header>
+                    <h2>10 motivos para atrasar o pagamento dos seus boletos no final de ano</h2>
+
+                    <span>Categoria</span>
+                    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint delectus magnam natus non itaque nobis et saepe consequatur illum ab commodi quasi tempora explicabo velit laborum, necessitatibus vel quidem.</p>
+                </header>
+            </article>
+
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+            <article>
+                <div>
+
+                </div>
+                <header>
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis suscipit praesentium.</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                </p>
+            </article>
+
+            <div>
+                <a href="http://">previous</a>
+                <a href="http://">Next</a>
+            </div>
+        </section>
+
+    </main>
+
+    <aside>
+        <div>
+            <header>
+                <h3>Pesquise...</h3>
+                <form action="">
+                    <input type="text" name="" id="" placeholder="..."><i class="bi bi-search"></i>
+                </form>
+            </header>
+            <section>
+                <header>
+                    <h3>Últimos Artigos</h3>
+                </header>
+                <section>
+                    <article>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            <h4>Título do post, teste de título de post</h4>
+                            <p>Gilson M. G.  -  12/01/2022</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            <h4>Título do post, teste de título de post</h4>
+                            <p>Gilson M. G.  -  12/01/2022</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            <h4>Título do post, teste de título de post</h4>
+                            <p>Gilson M. G.  -  12/01/2022</p>
+                        </div>
+                    </article>
+                </section>
+            </section>
+            <div>
+                <header>
+                    <h3>Categorias</h3>
+                </header>
+                <ul>
+                    <li><a href="http://">Categoria 1</a></li>
+                    <li><a href="http://">Categoria 2</a></li>
+                    <li><a href="http://">Categoria 3</a></li>
+                </ul>
+            </div>
+        </div>
+    </aside>
+
+    <footer>
+        <div>
+            <div>
+                <header>
+                    <h3>Sobre</h3>
+                </header>
+            </div>
+            <div>
+                <header>
+                    <h3>Redes Sociais</h3>
+                </header>
+                <ul>
+                    <li><a href="http://"><i class="bi bi-linkedin"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div>
+                <header>
+                    <h3>Contato</h3>
+                </header>
+                <ul>
+                    <li><i class="bi bi-envelope"></i><a href="mailto:contato@meusite.com.br">contato@meusite.com.br</a></li>
+                    <li><i class="bi bi-telephone"></i><a href="tel:+5511999999999">+55 11 9 9999-9999</a></li>
+                </ul>
+                <ul>
+                    <li><a href="http://">Categoria 1</a></li>
+                    <li><a href="http://">Categoria 2</a></li>
+                    <li><a href="http://">Categoria 3</a></li>
+                    <li><a href="http://">Categoria 4</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <p><?= date('Y') ?> - Todos os direitos reservados</p>
+        </div>
+    </footer>
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
---
+++ pagina-artigo.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>blog - VIMMPag</title>
+</head>
+<body>
+    
+    <header>
+        <div>
+            <div>
+                <ul>
+                    <li><i class="bi bi-envelope"></i><a href="mailto:contato@meusite.com.br">contato@meusite.com.br</a></li>
+                    <li><i class="bi bi-telephone"></i><a href="tel:+5511999999999">+55 11 9 9999-9999</a></li>
+                </ul>
+                <ul>
+                    <li><a href="http://"><i class="bi bi-linkedin"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div>
+                <div>
+                    <a href="">
+                        <img src="" alt="">
+                    </a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="http://">Site</a></li>
+                        <li><a href="http://">Sobre</a></li>
+                        <li><a href="http://">Contato</a></li>
+                    </ul>
+                </nav>
+                <div>
+                    <a href="http://">
+                        Simule Agora
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div>
+        <h1>10 motivos para não atrasar o pagamento dos seus boletos no final do ano</h1>
+    </div>
+
+    <main>
+
+        <div>
+            <p>
+                <span>
+                    <img src="" alt="">
+                </span>
+                Giorgio M. D.  -  12/01/2022
+            </p>
+
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, eius quae possimus, laboriosam nisi repellat debitis explicabo consequuntur omnis officiis porro dolores vitae quia ad autem quibusdam nihil eos mollitia doloribus reprehenderit libero? Veritatis, voluptatum harum dolorum praesentium fugit officiis.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, aperiam doloribus eius est rem voluptatum error earum? A modi magnam voluptatem obcaecati maxime in. Cumque similique tempore vitae animi molestias.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae voluptate enim nesciunt laborum itaque accusamus.</p>
+        <img src="" alt="">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptates sapiente, ducimus eum maxime sunt quod. Ab, earum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, fugiat amet magnam autem, incidunt vero, aliquid harum voluptate asperiores consequatur iste! Deserunt, cum labore!</p>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, suscipit earum?</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing.</li>
+        </ul>
+        
+        <hr>
+
+        <div>
+            <p>Compartilhe</p>
+            <ul>
+                <li><i class="bi bi-linkedin"></i></li>
+                <li><i class="bi bi-facebook"></i></li>
+                <li><i class="bi bi-whatsapp"></i></li>
+                <li><i class="bi bi-link-45deg"></i></li>
+            </ul>
+        </div>
+
+        <div>
+            <header>
+                <h3>Artigos Relacionados</h3>
+            </header>
+
+            <section>
+                <article>
+                    <div>
+
+                    </div>
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor adipisci rem nostrum?</h4>
+                    <p>
+                        <span>
+                            <img src="" alt="">
+                        </span>
+                        Georgio G. M.  -  12/02/2022
+                    </p>
+                </article>
+                <article>
+                    <div>
+
+                    </div>
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor adipisci rem nostrum?</h4>
+                    <p>
+                        <span>
+                            <img src="" alt="">
+                        </span>
+                        Georgio G. M.  -  12/02/2022
+                    </p>
+                </article>
+                <article>
+                    <div>
+
+                    </div>
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor adipisci rem nostrum?</h4>
+                    <p>
+                        <span>
+                            <img src="" alt="">
+                        </span>
+                        Georgio G. M.  -  12/02/2022
+                    </p>
+                </article>
+            </section>
+        </div>
+
+    </main>
+
+    <footer>
+        <div>
+            <div>
+                <header>
+                    <h3>Sobre</h3>
+                </header>
+            </div>
+            <div>
+                <header>
+                    <h3>Redes Sociais</h3>
+                </header>
+                <ul>
+                    <li><a href="http://"><i class="bi bi-linkedin"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="http://"><i class="bi bi-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div>
+                <header>
+                    <h3>Contato</h3>
+                </header>
+                <ul>
+                    <li><i class="bi bi-envelope"></i><a href="mailto:contato@meusite.com.br">contato@meusite.com.br</a></li>
+                    <li><i class="bi bi-telephone"></i><a href="tel:+5511999999999">+55 11 9 9999-9999</a></li>
+                </ul>
+                <ul>
+                    <li><a href="http://">Categoria 1</a></li>
+                    <li><a href="http://">Categoria 2</a></li>
+                    <li><a href="http://">Categoria 3</a></li>
+                    <li><a href="http://">Categoria 4</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <p><?= date('Y') ?> - Todos os direitos reservados</p>
+        </div>
+    </footer>
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
