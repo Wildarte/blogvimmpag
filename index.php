62776d0f87b84b54df1025e4132395e053a43958
wildarte <wildarte10@gmail.com>
2022-01-23 17:38:36 -0300

    quase finalizada a home, comecei a fazer a page de post

--- index.php
+++ index.php
@@ -1,53 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>blog - VIMMPag</title>
-</head>
-<body>
+
+<?php include('./header.php') ?>
     
-    <header class="header container-full">
-        <div class="container-full header_content">
-            <div class="header_over container-full">
-                <div class="container header_over_container">
-                    <ul class="header_over_contato">
-                        <li><a href="mailto:contato@meusite.com.br"><i class="bi bi-envelope"></i>contato@meusite.com.br</a></li>
-                        <li><a href="tel:+5511999999999"><i class="bi bi-telephone"></i> +55 11 9 9999-9999</a></li>
-                    </ul>
-                    <ul class="header_over_social">
-                        <li class="icon-linkedin"><a href="http://"><i class="bi bi-linkedin"></i></a></li>
-                        <li class="icon-facebook"><a href="http://"><i class="bi bi-facebook"></i></a></li>
-                        <li class="icon-instagram"><a href="http://"><i class="bi bi-instagram"></i></a></li>
-                    </ul>
-                </div>     
-            </div>
-            <div class="header_bar container-full">
-                <div class="header_bar_content container">
-                    <div class="header_link_logo">
-                        <a href="">
-                            <img src="./assets/img/logo.png" alt="">
-                        </a>
-                    </div>
-                    <nav class="menu">
-                        <ul>
-                            <li><a href="http://">Site</a></li>
-                            <li><a href="http://">Sobre</a></li>
-                            <li><a href="http://">Contato</a></li>
-                        </ul>
-                    </nav>
-                    <div class="header_cta">
-                        <a href="http://">
-                            Simule Agora
-                        </a>
-                    </div>
-                </div>   
-            </div>
-        </div>
-    </header>
     <div class="home_intro">
         <h1 class="home_title">Blog</h1>
     </div>
@@ -57,8 +10,8 @@
         <section class="section_posts">
             <div class="post_destaque">
                 <article class="card_post top_post">
-                    <div class="post_image" style="background-image: url('./assets/img/posts/post1.jpg')"></div>
-                    <header class="post_header">
+                    <div class="post_image_top" style="background-image: url('./assets/img/posts/post1.jpg')"></div>
+                    <header class="post_header_top">
                         <h2>10 motivos para não atrasar o pagamento dos seus boletos no final de ano</h2>
 
                         <span class="post_categoria">Categoria</span>
@@ -70,81 +23,109 @@
             </div>
             
 
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post2.jpg');">
+            <article class="single_card card_post">
+                <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post2.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
-                </header>
-                <p class="post_resume">
+                    <p class="post_resume">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+                    </p>
+                </header>
+                
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post3.jpg');">
+            <article class="single_card card_post">
+                <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post3.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+                
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post4.jpg');">
+            <article class="single_card card_post">
+                <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post4.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+               
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post1.jpg');">
+            <article class="single_card card_post">
+            <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post1.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+                
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post2.jpg');">
+            <article class="single_card card_post">
+            <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post2.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+                
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post3.jpg');">
+            <article class="single_card card_post">
+            <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post3.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+               
                 <a class="post_link" href="http://"></a>
             </article>
-            <article class="card_post">
-                <div class="post_image" style="background-image: url('./assets/img/posts/post4.jpg');">
+            <article class="single_card card_post">
+            <div class="post_image">
+                    <div class="post_image_content" style="background-image: url('./assets/img/posts/post4.jpg');">
+
+                    </div>
                 </div>
                 <header class="post_header">
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique debitis.</h2>
+                    <p class="post_resume">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
+                    </p>
                 </header>
-                <p class="post_resume">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dicta expedita nobis modi sint maiores vitae ex adipisci. Ratione, dolorem.
-                </p>
+                
                 <a class="post_link" href="http://"></a>
             </article>
 
@@ -170,7 +151,7 @@
                     <header class="sidebar_posts_header">
                         <h3>Últimos Artigos</h3>
                     </header>
-                    <section>
+                    <section class="sidebar_list_post">
                         <article class="sidebar_card_post">
                             <div class="sidebar_card_post_image" style="background-image: url('./assets/img/posts/post-side1.jpg');">
                             </div>
@@ -215,56 +196,6 @@
 
     </main>
 
-    <footer class="footer container-full">
-        <div class="footer_content container">
-            <div class="col footer_col_1">
-                <header class="header_footer_col_1">
-                    <h3>Sobre</h3>
-                </header>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, corporis sunt doloremque accusantium est fuga nesciunt voluptatem facere soluta eligendi aut? Commodi.</p>
-            </div>
-            <div class="col footer_col_2">
-                <header class="header_footer_col_2">
-                    <h3>Redes Sociais</h3>
-                </header>
-                <ul class="footer_list_social">
-                    <li class="icon-linkedin"><a href="http://"><i class="bi bi-linkedin"></i></a></li>
-                    <li class="icon-facebook"><a href="http://"><i class="bi bi-facebook"></i></a></li>
-                    <li class="icon-instagram"><a href="http://"><i class="bi bi-instagram"></i></a></li>
-                </ul>
-            </div>
-            <div class="col footer_col_3">
-                <header class="header_footer_col_3">
-                    <h3>Contato</h3>
-                </header>
-                <ul class="footer_contato">
-                    <li><a href="mailto:contato@meusite.com.br"><i class="bi bi-envelope"></i> contato@meusite.com.br</a></li>
-                    <li><a href="tel:+5511999999999"><i class="bi bi-telephone"></i> +55 11 9 9999-9999</a></li>
-                </ul>
-                <ul class="footer_categoria">
-                    <li><a href="http://">Categoria 1</a></li>
-                    <li><a href="http://">Categoria 2</a></li>
-                    <li><a href="http://">Categoria 3</a></li>
-                    <li><a href="http://">Categoria 4</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="footer_copy">
-            <p><?= date('Y') ?> - Todos os direitos reservados</p>
-        </div>
-    </footer>
+    
+<?php include('./footer.php') ?>
 
-    <script src="./assets/js/script.js"></script>
-
-    <script>
-        const btn_ctr = document.querySelectorAll('.btn_controll_posts');
-        btn_ctr.forEach((btn) => {
-            btn.addEventListener('click', (e) => {
-                e.preventDefault();
-            })
-        })
-        
-    </script>
-
-</body>
-</html>
